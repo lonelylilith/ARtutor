f7804d433ba8681973f11040109c3ca0de6d50fc
Kai <artemyplay@mail.ru>
2024-12-08 21:19:08 +0300

    v1
    v1

---
+++ Assets/menu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Button exit;
+
+
+
+    void Start()
+    {
+        
+    }
+    private void ExitGame()
+    {
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        UnityEngine.Application.Quit();
+    #endif
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
